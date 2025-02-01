7ae790e902d8acc974d7a86bc9d9da6872552ae3
Dawn Cheairs <dawnsusanne@gmail.com>
2025-02-01 10:42:24 -0700

    Fixed a problematic path in .json, added middleware

--- server/index.js
+++ server/index.js
@@ -5,10 +5,25 @@ const PORT = process.env.PORT || 3000;
 const express = require('express');
 const path = require('path');
 const app = express();
+const routes = require('./routes');
 
+// middleware
+app.use('/', routes);
+
+//  more middleware, but for handling errors
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Log the error stack trace
+    res.status(500).send('Something went wrong!');
+});
+
+
+// view engine
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));  // serve files from the public directory
 
+// serve static files from the public folder
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// start the server
 app.listen(PORT, () => {
-    console.log(`Server is operational on port ${PORT}`);
+    console.log(`Server is climbing mountains on port ${PORT}`);
 });
